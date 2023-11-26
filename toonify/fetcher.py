eb759f3d584c5e88f04f189081c402f719917e40
MukilanMuthu <mukilanmuthu@gmail.com>
2023-11-27 00:34:39 +0530

    removed example from fetcher

---
+++ toonify/fetcher.py
@@ -0,0 +1,12 @@
+import requests
+
+API_URL = "https://xdwvg9no7pefghrn.us-east-1.aws.endpoints.huggingface.cloud"
+headers = {
+	"Accept": "image/png",
+	"Authorization": "Bearer VknySbLLTUjbxXAXCjyfaFIPwUTCeRXbFSOjwRiCxsxFyhbnGjSFalPKrpvvDAaPVzWEevPljilLVDBiTzfIbWFdxOkYJxnOPoHhkkVGzAknaOulWggusSFewzpqsNWM",
+	"Content-Type": "application/json"
+}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
